76ce2e54a6e5c3ba77e155bfc363b7de0c5b7dbb
Xuân Trường <alonesera1612@gmail.com>
2025-03-10 07:57:31 +0700

    Update tinhtongtrenbang.c

--- tinhtongtrenbang/tinhtongtrenbang.c
+++ tinhtongtrenbang/tinhtongtrenbang.c
@@ -1,3 +1,7 @@
+/*Có bạn đứng thành vòng tròn trên sân, mỗi bạn cầm một bảng và cô giáo đứng ở giữa vòng tròn.
+*Khi cô giáo đọc một số thì các bạn đó viết lên bảng số đấy.
+*/Bạn hãy cho biết tổng của tất cả các số được ghi trên bảng là bao nhiêu.
+
 #include <stdio.h>
 
 int main () {
