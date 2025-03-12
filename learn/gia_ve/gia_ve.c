51c9903ee1b46fdd8026a4522e6b0083e61c9659
truong <alonesera1612@gmail.com>
2025-03-12 15:11:11 +0000

    comment code gia_ve.c

--- learn/gia_ve/gia_ve.c
+++ learn/gia_ve/gia_ve.c
@@ -4,15 +4,17 @@
 // phong cách tự định nghĩa hết, không nên áp dụng
 #define GIA_CHU_NHAT 10
 #define GIA_NGAY_THUONG 7
-#define GIAM_GIA_TRE_EM 0.5  // giảm 50% cho trẻ em =))
+#define GIAM_GIA_TRE_EM 0.5 // giảm 50% cho trẻ em =))
 #define VE_NGUOI_LON 0
 #define VE_TRE_EM 1
 #define NGAY_THUONG 0
 #define CHU_NHAT 1
 #define STR_CHU_NHAT "chu nhat"
 #define STR_NGAY_THUONG "ngay thuong"
+
 double tinh_tien(int so_luong, int loai_ve, int ngay_xem) {
     double gia_ve;
+    // nếu ngày xem là chủ nhật (1) thì giá vé là chủ nhật (10), ...
     if (ngay_xem == CHU_NHAT) {
         gia_ve = GIA_CHU_NHAT;
     } else {
@@ -27,11 +29,11 @@ int main() {
     char ten_phim[50];
     int so_luong, loai_ve, ngay_xem;
     double tong_tien;
-    const char *ngay_xem_str;
+    const char *ngay_xem_str;  // ngày xem là ngày nào (pointer)
 
     printf("Nhap ten phim: ");
-    fgets(ten_phim, sizeof(ten_phim), stdin);
-    ten_phim[strcspn(ten_phim, "\n")] = 0;
+    fgets(ten_phim, sizeof(ten_phim), stdin); // fgets: ghi được kí tự " "
+    ten_phim[strcspn(ten_phim, "\n")] = 0;    // tìm và xóa dấu xuống dòng
 
     printf("Nhap so luong ve: ");
     scanf("%d", &so_luong);
@@ -42,7 +44,7 @@ int main() {
     printf("Nhap ngay xem (%d: Chu nhat, %d: Ngay thuong): ", CHU_NHAT, NGAY_THUONG);
     scanf("%d", &ngay_xem);
 
-    // nếu 1 thì chủ nhật ...
+    // nếu 1 thì chủ nhật gán chủ nhủ nhật, ...
     if (ngay_xem == CHU_NHAT) {
         ngay_xem_str = STR_CHU_NHAT;
     } else {
