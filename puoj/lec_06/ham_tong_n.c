a355cfc558a1e21a029b3b6075594fc027d87581
Xuân Trường <alonesera1612@gmail.com>
2025-03-14 11:58:08 +0700

    Create ham_tong_n.c

---
+++ puoj/lec_06/ham_tong_n.c
@@ -0,0 +1,18 @@
+// Hàm tổng n
+
+#include <stdio.h>
+
+long long tong(int n){
+    long long tong(int n){
+        for (int i = 1; i <= n; i++);
+        return sum;
+    }
+}
+
+int main () {
+    int n;
+    scanf("%d",&n)
+    long long sum = tong(n);
+    printf("%d", n);
+    return 0;
+}
