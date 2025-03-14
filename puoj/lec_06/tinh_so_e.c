2e9d1c43c0256873ba3c37fa7872ad5a6afde81c
Xuân Trường <alonesera1612@gmail.com>
2025-03-14 10:52:19 +0700

    Update tinh_so_e.c

--- puoj/lec_06/tinh_so_e.c
+++ puoj/lec_06/tinh_so_e.c
@@ -1,25 +1,23 @@
-// Tính số e
-
 #include <stdio.h>
 #include <math.h>
 
 int main () {
     int n = 0;
-    long long  gt = 1;
+    long long  giai_thua = 1;
     double eps;
     double e = 1;
 
     printf ("Nhap so epsilon: ");
     scanf("%f", &eps);
 
-    while(1.0/gt > eps){
+    while(1.0/giai_thua > eps){
         n = n + 1;
-        gt = gt * n;
-        e = e + 1.0 / gt;
+        giai_thua = giai_thua * n;
+        e = e + 1.0 / giai_thua;
     }
 
     printf("GIa tri so e la %lf: \n", e );
     printf("GIa tri so e thuc te %lf: ", exp(1) );
-  
+
     return 0;
 }
