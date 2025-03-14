c7848ae571d24af86c96121fbe3bbced2ddf220d
truong <alonesera1612@gmail.com>
2025-03-14 15:17:20 +0000

    lec_6

--- puoj/lec_06/tinh_so_e/tinh_so_e.c
+++ puoj/lec_06/tinh_so_e/tinh_so_e.c
@@ -1,18 +1,18 @@
 // Tính số e
 
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
-int main () {
+int main() {
     int n = 0;
     long long giai_thua = 1;
     double epsilon;
     double e = 1;
 
-    printf ("Nhap so epsilon: ");
+    printf("Nhap so epsilon: ");
     scanf("%f", &epsilon);
 
-    while(1.0/giai_thua > epsilon){
+    while (1.0 / giai_thua > epsilon) {
         n = n + 1;
         giai_thua = giai_thua * n;
         e = e + 1.0 / giai_thua;
