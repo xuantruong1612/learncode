83ffc6b24e2a98d9daea229f5046927b9753ec98
Xuân Trường <alonesera1612@gmail.com>
2025-03-14 12:07:59 +0700

    Create ham_giai_thua.c

---
+++ puoj/lec_06/ham_giai_thua.c
@@ -0,0 +1,20 @@
+// Hàm giai thừa (đệ quy)
+
+#include <stdio.h>
+
+long long  giaithua(int n){
+    if (n == 0){
+        return 1;
+    }
+    else {
+        return giaithua (n -1) * n;
+    }
+}
+
+int main () {
+    int n;
+    scanf("%d", &n);
+    long long gt = giaithua(n);
+    printf("%d! = %lld", n, gt);
+    return 0;
+}
