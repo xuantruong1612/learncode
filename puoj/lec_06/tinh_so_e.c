995c0d5a1003300180d7b242f692bd5f40b95c61
Xuân Trường <alonesera1612@gmail.com>
2025-03-14 10:54:24 +0700

    Update tinh_so_e.c

--- puoj/lec_06/tinh_so_e.c
+++ puoj/lec_06/tinh_so_e.c
@@ -3,21 +3,21 @@
 
 int main () {
     int n = 0;
-    long long  giai_thua = 1;
+    long long giai_thua = 1;
     double eps;
     double e = 1;
 
-    printf ("Nhap so epsilon: ");
-    scanf("%f", &eps);
+    printf("Nhap so epsilon: ");
+    scanf("%lf", &eps);
 
-    while(1.0/giai_thua > eps){
+    while(1.0 / giai_thua > eps) {
         n = n + 1;
         giai_thua = giai_thua * n;
         e = e + 1.0 / giai_thua;
     }
 
-    printf("GIa tri so e la %lf: \n", e );
-    printf("GIa tri so e thuc te %lf: ", exp(1) );
+    printf("Gia tri so e duoc tinh gan: %lf\n", e);
+    printf("Gia tri so e thuc te: %lf\n", exp(1));
 
     return 0;
 }
