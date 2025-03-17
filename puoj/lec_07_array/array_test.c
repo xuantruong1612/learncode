1231cb04d66c58fc151150b73fa6bf397902e3ae
Xuân Trường <alonesera1612@gmail.com>
2025-03-17 08:00:28 +0700

    Update array_test.c

--- puoj/lec_07_array/array_test.c
+++ puoj/lec_07_array/array_test.c
@@ -3,9 +3,8 @@
 int main () {
     int a[5] = {1, 2, 3, 4, 5};
     for (int i = 0; i < 5; i++){
-        printf("a[%d] = %d", i a[i]);
+        printf("a[%d] = %d\n", i, a[i]);
     }
-    
 
     return 0;
 }
