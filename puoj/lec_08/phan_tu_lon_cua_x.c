38961a85867733c90a807c2417f5b080ea19fd0c
Xuân Trường <alonesera1612@gmail.com>
2025-03-21 10:18:03 +0700

    Create  phan_tu_lon_cua_x.c

---
+++ puoj/lec_08/phan_tu_lon_cua_x.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void in(int a[], int n){
+    for (int i = 0; i < n; i++)
+    scanf("%d", &a[i]);
+}
+
+int phan_tu_lon_x (int a[], int n, int x){
+    int dem = 0;
+    for (int i = 0; i < n; i++){
+        if (a[i] > x){
+            dem++;
+        }
+    }
+    return dem;
+}
+
+int main () {
+    int n, x;
+    scanf("%d%d", &n, &x);
+    int a[n];
+    in (a, n);
+    printf("%d ", phan_tu_lon_x (a, n, x));
+    
+    
+    return 0;
+}
