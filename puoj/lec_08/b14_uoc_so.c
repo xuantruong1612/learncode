3ea0546d28c15d3498ee6bcb1bfb9b6ef493a0aa
Xuân Trường <alonesera1612@gmail.com>
2025-03-21 11:15:21 +0700

    Create b14_uoc_so.c

---
+++ puoj/lec_08/b14_uoc_so.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void uoc (int n) {
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            printf("%d ", i);
+        }
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    uoc(n);
+    return 0;
+}
