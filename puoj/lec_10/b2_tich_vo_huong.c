befc9658d1c90f51a09d532bc03e59dcbc8a1605
Xuân Trường <alonesera1612@gmail.com>
2025-03-26 11:07:46 +0700

    Create b2_tich_vo_huong.c

---
+++ puoj/lec_10/b2_tich_vo_huong.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int tich_vo_huong(int n, int x[1000], int y [1000]){
+    int tich = 0;
+    for (int i = 0; i < n; i++){
+        scanf("%d", &x[i]);
+    }
+    for (int i = 0; i < n; i++){
+        scanf("%d", &y[i]);
+    }
+    for (int i = 0; i < n; i++){
+        tich += x[i] * y[i];
+    }
+    return tich;
+}
+
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int x[1000]; int y[1000];
+    printf("%d", tich_vo_huong(n, x, y));
+    
+    return 0;
+}
