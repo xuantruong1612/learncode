0ce18b0d317c2672e419b3fc37f4318049caec43
Xuân Trường <alonesera1612@gmail.com>
2025-04-04 11:06:55 +0700

    Update b1_thi_sinh_trung_tuyen.c

--- puoj/lec_14/b1_thi_sinh_trung_tuyen.c
+++ puoj/lec_14/b1_thi_sinh_trung_tuyen.c
@@ -1,48 +1,53 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-#define MAX_DONG 100
-#define MAX_THISINH 100
-
 typedef struct {
-    int maso;
+    int ma_so;
     int diem;
-    char hoten[MAX_DONG];
-    char quequan[MAX_DONG];
+    char ho_ten[100];
+    char que_quan[100];
 } ThiSinh;
 
 int main() {
-    FILE *tep_vao = fopen("input.txt", "r");
-    FILE *tep_ra = fopen("output.txt", "w");
-    
-    if (!tep_vao || !tep_ra) {
-        printf("Loi mo tep!\n");
+
+    int n, diem_chuan;
+
+    FILE *input = fopen("input.txt", "r");
+    if (input == NULL) {
+        printf("Không thể mở file input.txt\n");
         return 1;
     }
-    
-    int n, diemchuan, so_trung_tuyen = 0;
-    ThiSinh ds_thisinh[MAX_THISINH], ds_trungtuyen[MAX_THISINH];
-    
-    fscanf(tep_vao, "%d %d", &n, &diemchuan);
-    
+
+    fscanf(input, "%d %d", &n, &diem_chuan);
+
+    ThiSinh thi_sinh[n];
+    int dem_trung_tuyen = 0;
+
     for (int i = 0; i < n; i++) {
-        fscanf(tep_vao, "%d %d", &ds_thisinh[i].maso, &ds_thisinh[i].diem);
-        fscanf(tep_vao, " %[^\n]", ds_thisinh[i].hoten);
-        fscanf(tep_vao, " %[^\n]", ds_thisinh[i].quequan);
-        
-        if (ds_thisinh[i].diem >= diemchuan) {
-            ds_trungtuyen[so_trung_tuyen++] = ds_thisinh[i];
+        fscanf(input, "%d %d %s %s", &thi_sinh[i].ma_so, &thi_sinh[i].diem, thi_sinh[i].ho_ten, thi_sinh[i].que_quan);
+
+        if (thi_sinh[i].diem >= diem_chuan) {
+            dem_trung_tuyen++;
         }
     }
-    
-    fprintf(tep_ra, "Tong so thi sinh trung tuyen: %d\n", so_trung_tuyen);
-    for (int i = 0; i < so_trung_tuyen; i++) {
-        fprintf(tep_ra, "%d %d %s %s\n", ds_trungtuyen[i].maso, ds_trungtuyen[i].diem, ds_trungtuyen[i].hoten, ds_trungtuyen[i].quequan);
+
+    fclose(input);
+
+    FILE *output = fopen("output.txt", "w");
+    if (output == NULL) {
+        printf("Không thể mở file output.txt\n");
+        return 1;
     }
-    
-    fclose(tep_vao);
-    fclose(tep_ra);
-    
+
+    fprintf(output, "Tong so thi sinh trung tuyen: %d\n", dem_trung_tuyen);
+
+    for (int i = 0; i < n; i++) {
+        if (thi_sinh[i].diem >= diem_chuan) {
+            fprintf(output, "%d %d %s %s\n", thi_sinh[i].ma_so, thi_sinh[i].diem, thi_sinh[i].ho_ten, thi_sinh[i].que_quan);
+        }
+    }
+
+    fclose(output);
+
     return 0;
 }
