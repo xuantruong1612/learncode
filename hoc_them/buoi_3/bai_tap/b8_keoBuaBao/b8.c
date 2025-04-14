d7cbc363fc24c7202072f0c4e1950f547c69e8b0
truong <alonesera1612@gmail.com>
2025-04-14 19:05:45 +0700

    fix

--- hoc_them/buoi_3/bai_tap/b8_keoBuaBao/b8.c
+++ hoc_them/buoi_3/bai_tap/b8_keoBuaBao/b8.c
@@ -3,47 +3,47 @@
 #include <time.h>
 
 int main() {
-  int choice, may;
+    int choice, may;
 
-  srand(time(NULL));
+    srand(time(NULL));
 
-  printf("\n1 - KEO\n");
-  printf("2 - BUA\n");
-  printf("3 - LA\n");
-  printf("ANY OTHER KEY - EXIT\n");
-  printf("YOUR CHOICE: ");
+    printf("\n1 - KEO\n");
+    printf("2 - BUA\n");
+    printf("3 - LA\n");
+    printf("ANY OTHER KEY - EXIT\n");
+    printf("YOUR CHOICE: ");
 
-  if (scanf("%d", &choice) != 1 || choice < 1 || choice > 3) {
-    printf("Goodbye!\n");
+    if (scanf("%d", &choice) != 1 || choice < 1 || choice > 3) {
+        printf("Goodbye!\n");
+        return 0;
+    }
+
+    may = rand() % 3 + 1;
+    printf("\n");
+    printf("ENGINE CHOICE: ");
+    if (may == 1)
+        printf("KEO\n");
+    else if (may == 2)
+        printf("BUA\n");
+    else
+        printf("LA\n");
+
+    printf("YOUR CHOICE: ");
+    if (choice == 1)
+        printf("KEO\n");
+    else if (choice == 2)
+        printf("BUA\n");
+    else
+        printf("LA\n");
+    printf("------------------\n");
+    if (choice == may) {
+        printf("RESULT: TIE\n");
+    } else if ((choice == 1 && may == 3) || (choice == 2 && may == 1) ||
+               (choice == 3 && may == 2)) {
+        printf("RESULT: YOU WIN\n");
+    } else {
+        printf("RESULT: YOU LOSE\n");
+    }
+
     return 0;
-  }
-
-  may = rand() % 3 + 1;
-  printf("\n");
-  printf("ENGINE CHOICE: ");
-  if (may == 1)
-    printf("KEO\n");
-  else if (may == 2)
-    printf("BUA\n");
-  else
-    printf("LA\n");
-
-  printf("YOUR CHOICE: ");
-  if (choice == 1)
-    printf("KEO\n");
-  else if (choice == 2)
-    printf("BUA\n");
-  else
-    printf("LA\n");
-  printf("------------------\n");
-  if (choice == may) {
-    printf("RESULT: TIE\n");
-  } else if ((choice == 1 && may == 3) || (choice == 2 && may == 1) ||
-             (choice == 3 && may == 2)) {
-    printf("RESULT: YOU WIN\n");
-  } else {
-    printf("RESULT: YOU LOSE\n");
-  }
-
-  return 0;
 }
