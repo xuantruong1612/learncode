b0af56a121b156b309d8902ebdd4812c19bae01b
truong <alonesera1612@gmail.com>
2025-05-08 20:20:08 +0700

    câu 2 đề 30 đề thi cuối kì

--- learn/struct/on_tap/de_30_thi_cuoi_ki/cau_2.c
+++ learn/struct/on_tap/de_30_thi_cuoi_ki/cau_2.c
@@ -85,6 +85,23 @@ double sapxep(sanpham *sp, int n) {
     return tongsotien;
 }
 
+void loc(sanpham *sp, int n, double G1, double G2, char size[]) {
+    int dem = 0;
+    printf("--------------------------------------------------\n");
+    printf("%-5s|%-15s|%-10s|%-5s|%-20s\n", "STT", "TEN SAN PHAM", "SO LUONG",
+           "SIZE", "GIA BAN");
+    printf("--------------------------------------------------\n");
+    for (int i = 0; i < n; i++) {
+        if (sp[i].giaban >= G1 && sp[i].giaban <= G2 &&
+            strcmp(sp[i].size, size) == 0) {
+            printf("%-5d|%-15s|%-10d|%-5s|%-20.2lf\n", i + 1, sp[i].ten,
+                   sp[i].soluong, sp[i].size, sp[i].giaban);
+            printf("--------------------------------------------------\n"); dem++;
+        }
+    }
+    if (dem == 0) printf("Khong co san pham thoa man");
+}
+
 int main() {
     int n;
     printf("nhap so luong san pham: ");
@@ -94,5 +111,40 @@ int main() {
     nhap(sp, n);
     in(sp, n);
     printf("\ntong so tien la: %.2lf", sapxep(sp, n));
+    double G1, G2;
+    char size[4];
+    printf("\nnhap size va nhap gia:\n");
+    printf("nhap gia cua G1: ");
+    scanf("%lf", &G1);
+    printf("nhap gia cua G2: ");
+    scanf("%lf", &G2);
+    int s;
+    printf("moi nhap size:\n");
+    do {
+        printf("nhap size 1-S, 2-M, 3-L, 4-XL, 5-2XL: ");
+        scanf("%d", &s);
+        getchar();
+        if (s < 1 || s > 5) {
+            printf("khong co du lieu\n");
+        }
+    } while (s < 1 || s > 5);
+    switch (s) {
+    case 1:
+        strcpy(size, "S");
+        break;
+    case 2:
+        strcpy(size, "M");
+        break;
+    case 3:
+        strcpy(size, "L");
+        break;
+    case 4:
+        strcpy(size, "XL");
+        break;
+    case 5:
+        strcpy(size, "2XL");
+        break;
+    }
+    loc(sp, n, G1, G2, size);
     return 0;
 }
