6d281f103fd3c8c5a0d158742a1fae0cc2ed0776
Xuân Trường <alonesera1612@gmail.com>
2025-05-10 15:44:50 +0700

    Update cau_2.c

--- learn/struct/on_tap/de_thi_cuoi_ki/de0/cau_2.c
+++ learn/struct/on_tap/de_thi_cuoi_ki/de0/cau_2.c
@@ -1,155 +1,121 @@
-#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
 typedef struct {
-    char ten[10];
+    char ten[100];
     int soluong;
-    char size[5];
+    char kichco[4];
     double giaban;
 } sanpham;
 
-// 2.a
-void nhap(sanpham *sp, int n) {
-    for (int i = 0; i < n; i++) {
-        printf("nhap ten san pham thu %d: ", i + 1);
+int n;
+sanpham sp[1000];
+
+void task2a_nhap(){
+    printf("nhap so danh sach: ");
+    scanf("%d", &n);
+    getchar();
+
+    for (int i = 0; i < n; i++){
+        printf("nhap ten san pham thu %d: ", i+1);
         fgets(sp[i].ten, sizeof(sp[i].ten), stdin);
         sp[i].ten[strcspn(sp[i].ten, "\n")] = '\0';
 
-        printf("nhap so luong: ");
+        printf("nhap so luong san pham thu %d: ", i+1);
         scanf("%d", &sp[i].soluong);
-        getchar();
 
-        int s;
+        int size;
         do {
-            printf("nhap size 1-S, 2-M, 3-L, 4-XL, 5-2XL: ");
-            scanf("%d", &s);
-            getchar();
-            if (s < 1 || s > 5) {
-                printf("khong co du lieu\n");
-            }
-        } while (s < 1 || s > 5);
-        switch (s) {
-        case 1:
-            strcpy(sp[i].size, "S");
-            break;
-        case 2:
-            strcpy(sp[i].size, "M");
-            break;
-        case 3:
-            strcpy(sp[i].size, "L");
-            break;
-        case 4:
-            strcpy(sp[i].size, "XL");
-            break;
-        case 5:
-            strcpy(sp[i].size, "2XL");
-            break;
+            printf("nhap size san pham thu %d: ", i+1);
+            scanf("%d", &size);
+            if (size < 1 || size > 5) printf("sai, nhap lai\n");
+        } while (size < 1 || size > 5);
+        switch(size){
+            case 1: strcpy(sp[i].kichco, "S"); break;
+            case 2: strcpy(sp[i].kichco, "M"); break;
+            case 3: strcpy(sp[i].kichco, "L"); break;
+            case 4: strcpy(sp[i].kichco, "XL"); break;
+            case 5: strcpy(sp[i].kichco, "2XL"); break;
         }
-        printf("nhap gia ban: ");
+
+        printf("nhap gia ban thu %d: ", i+1);
         scanf("%lf", &sp[i].giaban);
         getchar();
     }
 }
 
-// 2.b
-void in(sanpham *sp, int n) {
-    printf("-----+---------------+----------+-----+----------+\n");
-    printf("%-5s|%-15s|%-10s|%-5s|%-20s\n", "STT", "TEN SAN PHAM", "SO LUONG",
-           "SIZE", "GIA BAN");
-    printf("-----+---------------+----------+-----+----------+\n");
-    for (int i = 0; i < n; i++) {
-        printf("%-5d|%-15s|%-10d|%-5s|%-20.2lf\n", i + 1, sp[i].ten,
-               sp[i].soluong, sp[i].size, sp[i].giaban);
-        printf("-----+---------------+----------+-----+----------+\n");
+void task2b_in(){
+    printf("+-----+--------------------+----------+-----+---------------+\n");
+    printf("|%-5s|%-20s|%-10s|%-5s|%-15s|\n", "STT","TEN SAN PHAM", "SO LUONG", "SIZE", "GIA BAN (k VND)");
+    for (int i = 0; i < n; i++){
+        printf("+-----+--------------------+----------+-----+---------------+\n");
+        printf("|%-5d|%-20s|%-10d|%-5s|%-15.2lf|\n", i+1, sp[i].ten, sp[i].soluong, sp[i].kichco, sp[i].giaban);
     }
+    printf("+-----+--------------------+----------+-----+---------------+\n");
 }
 
-// 2.c
-double sapxep(sanpham *sp, int n) {
-    double tongsotien = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (sp[i].soluong < sp[j].soluong) {
+double task2c_sapxep(){
+    double tongtien = 0;
+    for (int i = 0; i < n-1; i++){
+        for (int j = i + 1; j < n; j++){
+            if (sp[i].soluong < sp[j].soluong){
                 sanpham temp = sp[i];
                 sp[i] = sp[j];
                 sp[j] = temp;
             }
         }
-        tongsotien += sp[i].soluong * sp[i].giaban;
     }
-    printf("danh sach sau khi sap xep la: \n");
-    in(sp, n);
-    return tongsotien;
+    for (int i = 0; i < n; i++){
+        tongtien += sp[i].giaban * sp[i].soluong;
+    }
+    printf("\nsau khi sap xep\n");
+    task2b_in();
+    return tongtien;
 }
 
-// 2.d
-void loc(sanpham *sp, int n, double G1, double G2, char size[]) {
+void task2d_loc(double g1, double g2, char s[]){
     int dem = 0;
-    printf("--------------------------------------------------\n");
-    printf("%-5s|%-15s|%-10s|%-5s|%-20s\n", "STT", "TEN SAN PHAM", "SO LUONG",
-           "SIZE", "GIA BAN");
-    printf("--------------------------------------------------\n");
-    for (int i = 0; i < n; i++) {
-        if (sp[i].giaban >= G1 && sp[i].giaban <= G2 &&
-            strcmp(sp[i].size, size) == 0) {
-            printf("%-5d|%-15s|%-10d|%-5s|%-20.2lf\n", i + 1, sp[i].ten,
-                   sp[i].soluong, sp[i].size, sp[i].giaban);
-            printf("--------------------------------------------------\n");
+    printf("+-----+--------------------+----------+-----+---------------+\n");
+    printf("|%-5s|%-20s|%-10s|%-5s|%-15s|\n", "STT","TEN SAN PHAM", "SO LUONG", "SIZE", "GIA BAN (k VND)");
+    for (int i = 0; i < n; i++){
+        if (sp[i].giaban >= g1 && sp[i].giaban <= g2 && strcmp(sp[i].kichco, s) == 0){
+            printf("+-----+--------------------+----------+-----+---------------+\n");
+            printf("|%-5d|%-20s|%-10d|%-5s|%-15.2lf|\n", i+1, sp[i].ten, sp[i].soluong, sp[i].kichco, sp[i].giaban);
             dem++;
         }
     }
-    if (dem == 0)
-        printf("Khong co san pham thoa man");
+    printf("+-----+--------------------+----------+-----+---------------+\n");
+    if (!dem) printf("khong co du lieu\n");
 }
 
-int main() {
-    int n;
-    printf("nhap so luong san pham: ");
-    scanf("%d", &n);
-    getchar();
-    sanpham sp[1000];
-    nhap(sp, n);
-    in(sp, n);
-    printf("\ntong so tien la: %.2lf\n", sapxep(sp, n));
+int main(){
+    task2a_nhap();
+    task2b_in();
 
-    // 2.d
-    double G1, G2;
-    char size[4];
-    int s;
-    printf("moi nhap size:\n");
-    do {
-        printf("nhap size 1-S, 2-M, 3-L, 4-XL, 5-2XL: ");
-        scanf("%d", &s);
-        getchar();
-        if (s < 1 || s > 5) {
-            printf("khong co du lieu\n");
+    printf("\ntong so tien la: %.2lf\n", task2c_sapxep());
+    double g1, g2;
+    char s[4];
+
+    int size;
+        do {
+            printf("nhap size: ");
+            scanf("%d", &size);
+            if (size < 1 || size > 5) printf("sai, nhap lai\n");
+        } while (size < 1 || size > 5);
+
+        printf("nhap g1: ");
+        scanf("%lf", &g1);
+        printf("nhap g2: ");
+        scanf("%lf", &g2);
+
+        switch(size){
+            case 1: strcpy(s, "S"); break;
+            case 2: strcpy(s, "M"); break;
+            case 3: strcpy(s, "L"); break;
+            case 4: strcpy(s, "XL"); break;
+            case 5: strcpy(s, "2XL"); break;
         }
-    } while (s < 1 || s > 5);
+    task2d_loc(g1, g2, s);
 
-    // 2.e
-    printf("nhap gia cua G1: ");
-    scanf("%lf", &G1);
-    printf("nhap gia cua G2: ");
-    scanf("%lf", &G2);
-
-    switch (s) {
-        case 1:
-            strcpy(size, "S");
-            break;
-        case 2:
-            strcpy(size, "M");
-            break;
-        case 3:
-            strcpy(size, "L");
-            break;
-        case 4:
-            strcpy(size, "XL");
-            break;
-        case 5:
-            strcpy(size, "2XL");
-            break;
-    }
-    loc(sp, n, G1, G2, size);
     return 0;
 }
